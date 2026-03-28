2ca8d8ef0d3815b0e7a8c40d0dbbcf88d35f21c7
Uhupi <santino.lange@gmail.com>
2026-03-28 15:55:34 +0100

    Refactor styles and update font variables
    - Removed unused font weights (100, 200, 300) from _fonts.scss.
    - Removed unused variables ($accent, $spacing) from _variables.scss.
    - Adjusted letter-spacing in styles.scss for better readability.
    - Updated Webpack configuration to conditionally enable source maps based on the environment (development/production).
    - Improved CSS source maps for styles (desktop, mobile) to reflect recent changes.

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,31 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 
-module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
-  entry: {
-    main: './src/main.js',
-    styles: './src/styles.scss',
-    'styles.mobile': './src/styles.mobile.scss',
-    'styles.desktop': './src/styles.desktop.scss',
-  },
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, { loader: 'css-loader', options: { url: false, sourceMap: true } }, { loader: 'sass-loader', options: { sourceMap: true } }],
-      },
+module.exports = (_, argv) => {
+  const isDev = argv.mode === 'development';
+  return {
+    devtool: isDev ? 'source-map' : false,
+    entry: {
+      main: './src/main.js',
+      styles: './src/styles.scss',
+      'styles.mobile': './src/styles.mobile.scss',
+      'styles.desktop': './src/styles.desktop.scss',
+    },
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: '[name].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, { loader: 'css-loader', options: { url: false, sourceMap: isDev } }, { loader: 'sass-loader', options: { sourceMap: isDev } }],
+        },
+      ],
+    },
+    plugins: [
+      new RemoveEmptyScriptsPlugin(),
+      new MiniCssExtractPlugin({ filename: '[name].css' }),
     ],
-  },
-  plugins: [
-    new RemoveEmptyScriptsPlugin(),
-    new MiniCssExtractPlugin({ filename: '[name].css' }),
-  ],
+  };
 };
